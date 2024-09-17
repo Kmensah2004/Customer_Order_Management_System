56babb299a1b9bf2c585b7e6c2ca880ed963a836
Kosi Mensah <kmensah@usf.edu>
2024-09-16 22:12:15 -0400

    Initialize inventory with product objects

---
+++ customer_order_management_system.js
@@ -0,0 +1,5 @@
+//Task 1 array with 4 diffrent products from coffe shop
+const inventory = [{name: "coffee", price: 6.99, quantity:5},
+    {name: "Bagel", price: 5.99, quantity:11},
+    {name: "LemonBread", price: 1.50, quantity:15},
+    {name: "Lemonade", price: 3.99, quantity:8}];
