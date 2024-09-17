25e6646531febc3a30c9bb5e2a089ceedd34dc7c
Kosi Mensah <kmensah@usf.edu>
2024-09-17 15:12:43 -0400

    Initialize orders array

--- customer_order_management_system.js
+++ customer_order_management_system.js
@@ -3,3 +3,9 @@ const inventory = [{name: "coffee", price: 6.99, quantity:5},
     {name: "Bagel", price: 5.99, quantity:11},
     {name: "LemonBread", price: 1.50, quantity:15},
     {name: "Lemonade", price: 3.99, quantity:8}];
+
+    // Task 2
+    let orders = [];
+    const orderItems = [{Cname: "Jimmy",name:"coffee",price: 6.99, ordquantity:2,Status:Completed, 
+    Cname:"Carl",name:"LemonBread",price: 1.50, ordquantity:1, Status:Pending}];
+    
